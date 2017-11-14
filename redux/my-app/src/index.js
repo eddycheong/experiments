98b287aaaf8ed914c03e4b7379d3cf90345743c2
eddycheong <cheong.e49@gmail.com>
2017-11-14 14:45:22 -0800

    Saved the todo example from redux

--- redux/my-app/src/index.js
+++ redux/my-app/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import './index.css';
-import App from './App';
+import todoApp from './reducers'
+import App from './components/App'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(todoApp)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
